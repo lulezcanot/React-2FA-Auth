08ac817da552a55f37c9d54cbe03dc5ddb435a27
Luis Lezcano <llezcano@dtransforma.com>
2025-06-27 14:59:13 -0500

    Feature: add front-end and backend logic

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { RouterProvider } from "react-router-dom"
+import router from "./routes"
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-slate-900 h-screen'>
+      <div className='flex justify-center items-center h-screen'>
+        <RouterProvider router={router}/>
+      </div>
+    </div>
+  )
+}
+
+export default App
